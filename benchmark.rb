44ae63b0f9c6d348f7f4342bfb34a1bfc84fd469
Joseph HALTER <joseph.halter@kneip.com>
2010-07-24 13:44:48 +0200

    Add Sequel original benchs

--- benchmark.rb
+++ benchmark.rb
@@ -1,4 +1,5 @@
 require "benchmark"
+require "rubygems"
 require "bundler"
 Bundler.require :default
 
@@ -7,11 +8,45 @@ db_name = "mongobench"
 connection = Mongo::Connection.new
 db = connection.db(db_name)
 db.collections.each{|c| c.remove}
-db[:mongo_driver].create_index(:a)
 
 n = 1000
 
+puts "\n*** Sequel with SQLite ***"
+SQLITE = Sequel.sqlite('nosequel.sqlite')
+SQLITE.create_table!(:t) do
+  primary_key :_id
+  Integer :a
+  Integer :b
+end
+Benchmark.bm(25) do |x|
+  x.report("#{n} inserts:"){n.times{|i| SQLITE[:t].insert(:a=>i, :b=>n - i)}}
+  x.report("#{n} lookups by id:"){n.times{|i| SQLITE[:t][:a=>i]}}
+  x.report("#{n/25} select alls:"){(n/25).times{|i| SQLITE[:t].all}}
+  x.report("#{n} updates:"){n.times{|i| SQLITE[:t].filter(:a=>i).update(:b=>i)}}
+  x.report("#{n} deletes:"){n.times{|i| SQLITE[:t].filter(:a=>i).delete}}
+end
+
+puts "Garbage collecting..."
+GC.start
+
+puts "\n*** Sequel with Mongo ***"
+$:.unshift('sequel-mongo/lib')
+MONGO = Sequel.connect('mongo:///' + db_name)
+MONGO[:t].add_index :a
+Benchmark.bm(25) do |x|
+  x.report("#{n} inserts:"){n.times{|i| MONGO[:t].insert(:a=>i, :b=>n - i)}}
+  x.report("#{n} lookups by id:"){n.times{|i| MONGO[:t][:a=>i]}}
+  x.report("#{n/25} select alls:"){(n/25).times{|i| MONGO[:t].all}}
+  x.report("#{n} updates:"){n.times{|i| MONGO[:t].filter(:a=>i).update(:b=>i)}}
+  x.report("#{n} deletes:"){n.times{|i| MONGO[:t].filter(:a=>i).delete}}
+end
+
+puts "Garbage collecting..."
+db.collections.each{|c| c.remove}
+GC.start
+
 puts "\n*** Mongo ruby driver ***"
+db[:mongo_driver].create_index(:a)
 Benchmark.bm(25) do |x|
   x.report("#{n} inserts:"){n.times{|i| db[:mongo_driver].insert(:a => i, :b => n-i)}}
   x.report("#{n} lookups:"){n.times{|i| db[:mongo_driver].find_one(:a => i)}}
@@ -20,6 +55,11 @@ Benchmark.bm(25) do |x|
   x.report("#{n} deletes:"){n.times{|i| db[:mongo_driver].remove(:a => i)}}
 end
 
+puts "Garbage collecting..."
+db.collections.each{|c| c.remove}
+GC.start
+
+puts "\n*** MongoMapper ***"
 MongoMapper.connection = connection
 MongoMapper.database = db_name
 class MongoMapperObject
@@ -27,8 +67,6 @@ class MongoMapperObject
   key :a, Integer, :index => true
   key :b, Integer
 end
-
-puts "\n*** MongoMapper ***"
 Benchmark.bm(25) do |x|
   x.report("#{n} inserts:"){n.times{|i| MongoMapperObject.create!(:a => i, :b => n-i)}}
   x.report("#{n} lookups:"){n.times{|i| MongoMapperObject.first(:a => i)}}
@@ -37,6 +75,11 @@ Benchmark.bm(25) do |x|
   x.report("#{n} deletes:"){n.times{|i| MongoMapperObject.first(:a => i).destroy}}
 end
 
+puts "Garbage collecting..."
+db.collections.each{|c| c.remove}
+GC.start
+
+puts "\n*** Mongoid ***"
 Mongoid.configure do |config|
   config.master = db
 end
@@ -46,8 +89,6 @@ class MongoidObject
   field :b, :type => Integer
   index :a
 end
-
-puts "\n*** Mongoid ***"
 Benchmark.bm(25) do |x|
   x.report("#{n} inserts:"){n.times{|i| MongoidObject.create!(:a => i, :b => n-i)}}
   x.report("#{n} lookups:"){n.times{|i| MongoidObject.first(:conditions => {:a => i})}}
